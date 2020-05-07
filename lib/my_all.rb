861d773bae1aca92cf4b71917a9e05fa149a473f
Jennifer Hansen <jenn.leigh.hansen@gmail.com>
2020-05-07 15:58:23 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -3,3 +3,15 @@ require 'pry'
 def my_all?(collection)
 
 end
+
+def my_find(collection)
+  index = 0 
+  while index < collection.length 
+   # return collection[index] if yield(collection[index])
+    
+    if yield(collection[index])
+      return collection[index]
+    end
+     index += 1   
+  end
+end
